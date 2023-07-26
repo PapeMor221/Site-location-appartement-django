f84fb750be5301415e5da67ccddfffce729d61ca
PapeMor <ndiayepm@ept.sn>
2023-07-26 12:37:13 +0000

    requirements

--- PolyAppart/settings.py
+++ PolyAppart/settings.py
@@ -135,6 +135,7 @@ STATIC_URL = "static/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 STATICFILES_DIRS = [BASE_DIR/"static"]
+STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
--- README.md
+++
@@ -1,1 +0,0 @@
-# Site-location-appartement-django
